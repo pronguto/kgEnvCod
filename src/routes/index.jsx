c1eab7e50f687a9e212eab460033abcb9d746ff4
Gustavo Ribeiro Sergio <ribeiro.gustavo95@gmail.com>
2022-03-13 00:04:24 -0300

    Routes created

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -1,1 +1,10 @@
-export const Routes = () => {};
+import { Route, Switch } from "react-router-dom";
+import { Home } from "../pages/home";
+
+export const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/" component={Home} />
+    </Switch>
+  );
+};
