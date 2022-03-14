822a460a35ab65346a2d48d715988f59a66e5ecf
Gustavo Ribeiro Sergio <ribeiro.gustavo95@gmail.com>
2022-03-14 00:43:16 -0300

    fix background repeat

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,10 +5,11 @@ export const theme = extendTheme({
     global: {
       body: {
         bgGradient:
-          "linear(215.44deg,rgba(15, 15, 15, 0.91) 15.64%, rgba(80, 0, 0, 0.79) 63.96%, rgba(10, 0, 71, 0.8) 78.73%, rgba(10, 0, 71, 0.8) 78.73%) no-repet",
+          "linear(215.44deg,rgba(15, 15, 15, 0.91) 15.64%, rgba(80, 0, 0, 0.79) 63.96%, rgba(10, 0, 71, 0.8) 78.73%, rgba(10, 0, 71, 0.8) 78.73%) ",
         width: "100vw",
         height: "100vh",
         backgroundSize: "cover",
+        backgroundRepeat: "no",
       },
     },
   },
